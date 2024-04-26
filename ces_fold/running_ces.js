cffb6b860c87d33070c83dc18526e54382b3782b
ryalberti <ealberti@stevens.edu>
2024-04-26 05:41:22 -0400

    tried to move, wrote dropdown

--- ces_fold/running_ces.js
+++ ces_fold/running_ces.js
@@ -7,6 +7,23 @@ Cesium.Ion.defaultAccessToken =
 "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YWUyMWRmMS1jNDMwLTQxYTQtYTY4NC02YzViMDc4MzRhMGQiLCJpZCI6MjEwNTY2LCJpYXQiOjE3MTM4NDc3MDV9.tzUsn3XhbqICfw5zou-Qsk7XO_e7fqwAhnz0eKE9JlE";
 const viewer = new Cesium.Viewer("cesiumContainer", {shouldAnimate: true});
 
+// functions 
+function openFiles(){
+    window.open("file:///", '_blank'); // I doubt this'll work tbh 
+}
+
+function center(){
+    viewer.flyTo(pointEntity);
+}
+
+function center2(){
+    viewer.flyTo(pointEntity2)
+}
+
+function center7(){
+    viewer.flyTo(pointEntity3)
+}
+
 // code 
 
 // Static entity @ Howe
@@ -51,10 +68,11 @@ viewer.clock.clockRange = Cesium.ClockRange.LOOP_STOP;
 viewer.timeline.zoomTo(start, stop);
 
 // moving position 
-const position = new Cesium.SampledPositionProperty();
+const position5 = new Cesium.SampledPositionProperty();
 const startPosition = new Cesium.Cartesian3.fromDegrees(40.749,-74.034,70.000);
 const endPosition  = new Cesium.Cartesian3.fromDegrees(40.741,-74.025,91);
-const velocityVectorProperty = new Cesium.VelocityVectorProperty(position,false);
+const velocityVectorProperty = new Cesium.VelocityVectorProperty(position5,false);
+const velocityVector = new Cesium.Cartesian3();
 
 const numberOfSamples = 100;
 for (let i = 0; i <= numberOfSamples; ++i) {
@@ -64,18 +82,29 @@ for (let i = 0; i <= numberOfSamples; ++i) {
     factor * totalSeconds,
     new Cesium.JulianDate()
   );
-}
+
   // Lerp using a non-linear factor so that the vehicle accelerates.
-//   const locationFactor = Math.pow(factor, 2); 
-//   const location = Cesium.Cartesian3.lerp(
-//     startPosition,
-//     endPosition,
-//     locationFactor,
-//     new Cesium.Cartesian3()
-//   );
-//   position.addSample(time, location);
-  // Rotate the wheels based on how fast the vehicle is moving at each timestep.
-//   velocityVectorProperty.getValue(time, velocityVector);
+  const locationFactor = Math.pow(factor, 2); 
+  const location = Cesium.Cartesian3.lerp(
+    startPosition,
+    endPosition,
+    locationFactor,
+    new Cesium.Cartesian3()
+  );
+  position5.addSample(time, location);
+//   // Rotate the wheels based on how fast the vehicle is moving at each timestep.
+  velocityVectorProperty.getValue(time, velocityVector);
+}
+
+
+// // Add our vehicle model.
+const vehicleEntity = viewer.entities.add({
+    position: position5,
+    orientation: new Cesium.VelocityOrientationProperty(position5), // Automatically set the vehicle's orientation to the direction it's facing.
+    description: `Power: ${dataPoint3.power},\nFrequency: ${dataPoint3.freq}, \nBubble: ${dataPoint3.bubble}`,
+    point: { pixelSize: 10, color: Cesium.Color.RED },
+  });
+  
 
 // calls 
 viewer.flyTo(pointEntity);
